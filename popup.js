026c117700ea1f6e0a7368839de886b42f621340
OwaisSiddiqui <GitHubGIt18888*>
2021-02-21 00:59:29 -0500

    Added feature

--- popup.js
+++ popup.js
@@ -6,14 +6,14 @@ chrome.storage.sync.get("color", ({ color }) => {
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
 
 // The body of this function will be executed as a content script inside the
 // current page
@@ -22,3 +22,15 @@ function setPageBackgroundColor() {
     document.body.style.backgroundColor = color;
   });
 }
+
+// Open link in new tab
+// const openLinkInNewTab = (event) => {
+//   event.preventDefault()
+//   chrome.tabs.create({url: event.target.href, active: false});
+//   return false
+// }
+
+// Open all links with target=_blank in new tab
+// document.querySelector('a[target="_blank"]').addEventListener('click', (event) => {
+//   openLinkInNewTab(event)
+// })
