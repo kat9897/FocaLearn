7cb7366f1e4fb855f0f35228fa1ddabc1cf93300
OwaisSiddiqui <GitHubGIt18888*>
2021-02-21 03:09:19 -0500

    Added task manager (notes)

--- popup.js
+++ popup.js
@@ -50,3 +50,23 @@ function Start(song) {
       Restart(softMusic);
   });
   });
+
+const handleNotes = (event) => {
+  var notes = event.target.value
+  chrome.storage.local.set({"notes": notes})
+}
+
+document.addEventListener("DOMContentLoaded", async (event) => { 
+  var notes = document.getElementById("notes")
+  await chrome.storage.local.get("notes", (value) => {
+    var stored_notes = value.notes
+    if (typeof stored_notes == "string") {
+      notes.value = stored_notes
+    } else {
+      notes.value = ""
+    }
+  })
+  document.getElementById("notes").addEventListener("input", (event) => {
+    handleNotes(event)
+  })
+});
