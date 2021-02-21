59835be083cb0e7b5ebfbf20e732787595080318
ZixiaoR <61098486+ZixiaoR@users.noreply.github.com>
2021-02-21 01:16:24 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -6,14 +6,14 @@ chrome.storage.sync.get("color", ({ color }) => {
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
 
 // The body of this function will be executed as a content script inside the
 // current page
@@ -22,3 +22,31 @@ function setPageBackgroundColor() {
     document.body.style.backgroundColor = color;
   });
 }
+
+function Start(song) {
+  song.play();
+  }
+  
+  function Pause(song) {
+  song.pause();
+  }
+  
+  function Restart(song) {
+  song.currentTime = 0;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function(){
+  var Start_Music = document.getElementById('StartMusic');
+  var Pause_Music = document.getElementById('PauseMusic');
+  var Restart_Music = document.getElementById('RestartMusic');
+  
+  Start_Music.addEventListener('click', function() {
+      Start(softMusic);
+  });
+  Pause_Music.addEventListener('click', function() {
+      Pause(softMusic);
+  });
+  Restart_Music.addEventListener('click', function() {
+      Restart(softMusic);
+  });
+  });
